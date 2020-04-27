cf8a5b723c1d894c4e2d960582a210db1fcc4ac1
LukewarmCoffee <43965971+LukewarmCoffee@users.noreply.github.com>
2020-04-27 12:50:07 -0400

    sentence kado updates

--- lib/widgets/kados/sentence_kado_item.dart
+++ lib/widgets/kados/sentence_kado_item.dart
@@ -24,6 +24,13 @@ class _SentenceKadoItemState extends State<SentenceKadoItem> {
   bool kana = false;
   bool english = false;
   bool answer = false;
+  Map<String, bool> good = Map();
+
+  @override
+  void initState() {
+    for (String id in widget.kado.wordIds) good[id] = true;
+    super.initState();
+  }
 
   showKana() {
     setState(() {
@@ -43,6 +50,12 @@ class _SentenceKadoItemState extends State<SentenceKadoItem> {
     });
   }
 
+  switchGood(String id) {
+    setState(() {
+      good[id] = !good[id];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<WordsBloc, WordsState>(
@@ -54,7 +67,93 @@ class _SentenceKadoItemState extends State<SentenceKadoItem> {
               .map((wordId) =>
                   state.words.firstWhere((word) => word.id == wordId))
               .toList();
-          return Column(
+          return answer
+              ? Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        for (Word word in words)
+                          GestureDetector(
+                            child: good[word.id]
+                                ? Container(
+                                    child: Text(word.japanese),
+                                    color: Colors.greenAccent,
+                                  )
+                                : Container(
+                                    child: Text(word.japanese),
+                                    color: Colors.redAccent,
+                                  ),
+                            onTap: () => switchGood(word.id),
+                          )
+                        //Text(word.japanese),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        for (Word word in words) Text(word.kana)
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        for (Word word in words) Text(word.english + ' ')
+                      ],
+                    ),
+                    Container(child: Text(widget.kado.translation ?? '')),
+                    Row(
+                      children: <Widget>[
+                        RaisedButton(
+                          child: Text('continue'),
+                          onPressed: () {
+                            widget.nextPage();
+                            for (Word word in words)
+                              if (!good[word.id])
+                                BlocProvider.of<WordsBloc>(context).add(
+                                  UpdateWord(
+                                    word.copyWith(
+                                        confidence: word.confidence - 0.75),
+                                  ),
+                                );
+                              else if (good[word.id])
+                                BlocProvider.of<WordsBloc>(context).add(
+                                  UpdateWord(
+                                    word.copyWith(
+                                        confidence: word.confidence + 0.5),
+                                  ),
+                                );
+                          },
+                        ),
+                        RaisedButton(
+                          child: Text('good'),
+                          onPressed: () {
+                            widget.nextPage();
+                            for (Word word in words)
+                              BlocProvider.of<WordsBloc>(context).add(
+                                UpdateWord(
+                                  word.copyWith(
+                                      confidence: word.confidence + 0.5),
+                                ),
+                              );
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                )
+              : Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        for (Word word in words) Text(word.japanese),
+                      ],
+                    ),
+                    RaisedButton(
+                      child: Text('show answer'),
+                      onPressed: () => showAnswer(),
+                    )
+                  ],
+                );
+
+          /*Column(
             children: <Widget>[
               widget.kado.learned
                   ? Center(
@@ -136,7 +235,7 @@ class _SentenceKadoItemState extends State<SentenceKadoItem> {
                 ],
               ),
             ],
-          );
+          );*/
         } else {
           return Container();
         }
